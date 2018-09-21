025554e6a95349586f393ef04f503b4f3b948c3b
Steven Scaffidi <sscaff1@gmail.com>
2018-09-21 16:29:51 -0500

    initial commit

---
+++ assets/mathFunctions.example
@@ -0,0 +1,15 @@
+const add = (x, y) => x + y;
+
+const subtract = (x, y) => x - y;
+
+describe("Test our math functions", () => {
+  test("Numbers should add", () => {
+    expect(add(1, 2)).toBe(3);
+    expect(add(4, 5)).toBe(9);
+  });
+
+  test("Numbers should substract", () => {
+    expect(subtract(1, 2)).toBe(-1);
+    expect(subtract(7, 5)).toBe(2);
+  });
+});
--- presentation/index.js
+++ presentation/index.js
@@ -2,17 +2,7 @@
 import React from "react";
 
 // Import Spectacle Core tags
-import {
-  BlockQuote,
-  Cite,
-  Deck,
-  Heading,
-  ListItem,
-  List,
-  Quote,
-  Slide,
-  Text
-} from "spectacle";
+import { Deck, Heading, ListItem, List, Slide, Text, CodePane, Image } from "spectacle";
 
 // Import theme
 import createTheme from "spectacle/lib/themes/default";
@@ -20,51 +10,93 @@ import createTheme from "spectacle/lib/themes/default";
 // Require CSS
 require("normalize.css");
 
-const theme = createTheme({
-  primary: "white",
-  secondary: "#1F2022",
-  tertiary: "#03A9FC",
-  quaternary: "#CECECE"
-}, {
-  primary: "Montserrat",
-  secondary: "Helvetica"
-});
+const theme = createTheme(
+  {
+    primary: "white",
+    secondary: "#1F2022",
+    tertiary: "#03A9FC",
+    quaternary: "#CECECE"
+  },
+  {
+    primary: "Montserrat",
+    secondary: "Helvetica"
+  }
+);
 
 export default class Presentation extends React.Component {
   render() {
+    const headingProps = {
+      size: 1,
+      fit: true,
+      caps: true
+    };
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+      <Deck transition={["slide"]} transitionDuration={500} theme={theme}>
+        <Slide bgColor="primary">
+          <Heading {...headingProps} textColor="secondary">
+						Jest
           </Heading>
           <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+						Delightful JavaScript Testing
           </Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+        <Slide bgColor="tertiary">
+          <Heading {...headingProps} textColor="primary">
+						What is Jest?
+          </Heading>
+          <Text size={6} textColor="secondary" textAlign="left">
+            <List>
+              <ListItem>JavaScript Testing framework built by Facebook</ListItem>
+              <ListItem>Can be used for both unit tests and front-end integration testing</ListItem>
+              <ListItem>Easily mock functions to produce fast, reListItemable tests</ListItem>
+              <ListItem>Easily integrates with Webpack</ListItem>
+              <ListItem>Setup and tear down features before each test or blocks of test</ListItem>
+              <ListItem>
+								For the future, allows you take snapshots of React components to ensure UI remains consistent
+              </ListItem>
+            </List>
+          </Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
+        <Slide bgColor="secondary">
+          <Heading {...headingProps}>Example of Test</Heading>
+          <CodePane lang="javascript" source={require("raw-loader!../assets/mathFunctions.example")} textSize={20} />
         </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
+        <Slide bgColor="secondary">
+          <Heading {...headingProps}>Example Output</Heading>
+          <Image src="https://i.imgur.com/PvS1Dpa.png" height="450px" />
+        </Slide>
+        <Slide bgColor="tertiary">
+          <Heading {...headingProps} textColor="primary">
+						Implementation at LP
+          </Heading>
+          <Text size={6} textColor="secondary" textAlign="left">
+            <List>
+              <ListItem>JS located in the _client/ folder is modularized. Tests located in _client/__tests__</ListItem>
+              <ListItem>Folder structure in __tests__ match the folder structure in _client</ListItem>
+              <ListItem>For CI, we can use `npm run test` to run our tests</ListItem>
+              <ListItem>
+								During dev process, we can watch tests and they will automatically rerun for code change using `npm run
+								test-watch`
+              </ListItem>
+            </List>
+          </Text>
+        </Slide>
+        <Slide bgColor="secondary">
+          <Heading {...headingProps}>Demo</Heading>
+        </Slide>
+        <Slide bgColor="tertiary">
+          <Heading {...headingProps} textColor="primary">
+						Current State and Todo/Discuss
+          </Heading>
+          <Text size={6} textColor="secondary" textAlign="left">
+            <List>
+              <ListItem>Currently working on CI-53 branch. This branch will be eventually merged into KitSkus</ListItem>
+              <ListItem>Training around writing front-end tests?</ListItem>
+              <ListItem>All _client modules should include at least 1 unit test?</ListItem>
+              <ListItem>Integrate into our CI process? Take action on failure?</ListItem>
+              <ListItem>Establish githook to run tests before push?</ListItem>
+            </List>
+          </Text>
         </Slide>
       </Deck>
     );
